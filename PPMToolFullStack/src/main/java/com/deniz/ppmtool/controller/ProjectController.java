99f96af6ba7592bf258ccf4ba0e9b562d026091c
denizsalman <ddenizsalman@gmail.com>
2021-02-17 00:20:23 +0300

    Custom validation error

--- PPMToolFullStack/src/main/java/com/deniz/ppmtool/controller/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/deniz/ppmtool/controller/ProjectController.java
@@ -1,11 +1,16 @@
 package com.deniz.ppmtool.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +31,14 @@ public class ProjectController {
 			BindingResult bindingResult) {
 		
 		if (bindingResult.hasErrors()) {
-			return new ResponseEntity<String>("Invalid Project object", HttpStatus.BAD_REQUEST);
+			Map<String, String> errorMap = new HashMap<>();
+			
+			for (FieldError error : bindingResult.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			
+			return new ResponseEntity<Map<String, String>>(errorMap, 
+					HttpStatus.BAD_REQUEST);
 		}
 		
 		projectService.saveOrUpdate(project);
