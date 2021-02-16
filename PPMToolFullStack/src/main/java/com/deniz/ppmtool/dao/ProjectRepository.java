a27ab3ba98f61c3c0fb199f78bb96dc8e35dfcdc
denizsalman <ddenizsalman@gmail.com>
2021-02-16 14:04:31 +0300

    ProjectRepository @Repository

--- PPMToolFullStack/src/main/java/com/deniz/ppmtool/dao/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/com/deniz/ppmtool/dao/ProjectRepository.java
@@ -1,13 +1,11 @@
 package com.deniz.ppmtool.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.deniz.ppmtool.entity.Project;
 
+@Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
-
-	@Override
-	Iterable<Project> findAllById(Iterable<Long> ids);
-
 	
 }
